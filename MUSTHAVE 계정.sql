c7dbf6b6d725798db52ac6f69e08fa6800e8f841
iamstudybox00 <iamstudybox00@gmail.com>
2025-05-14 17:50:33 +0900

    13 - 파일업로드까지

--- MUSTHAVE 계정.sql
+++ MUSTHAVE 계정.sql
@@ -78,3 +78,16 @@ SELECT * FROM (
     ) Tb
 )
  WHERE rNum BETWEEN 1 AND 10;
+ 
+ create table myfile (
+    idx number primary key,
+    title varchar2(200) not null,
+    cate varchar2(100),
+    ofile varchar2(100) not null,
+    sfile varchar2(30) not null,
+    postdate date default sysdate not null
+);
+DESC myfile;
+
+DELETE * FROM myfile;
+SELECT * FROM myfile;
